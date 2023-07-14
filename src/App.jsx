95b340daf2b39ec6b9eab3f37413e3f7763b27a3
unknown <ulyamba@gmail.com>
2023-07-14 20:49:30 +0200

    user

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Profile from './components/Profile/Profile';
+import user from '../src/data/user.json';
+
+const App = () => {
+  return (
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
+};
+
+export default App;
