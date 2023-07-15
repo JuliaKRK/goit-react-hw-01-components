7736b6596e9600e3ddae38b7d124c9cad53a7ef9
unknown <ulyamba@gmail.com>
2023-07-15 21:33:02 +0200

    Profile-css

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './FriendListItem/FriendListItem.module.css';
+import styles from './FriendList.module.css';
 
 const FriendList = ({ friends }) => {
   return (
